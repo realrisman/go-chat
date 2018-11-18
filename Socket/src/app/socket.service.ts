95868e9b85566b019512c5df4b7f384135c4d1e4
Muhamad Risman <real.risman@gmail.com>
2018-11-18 08:58:39 +0700

    Fix import EventEmmiter

--- Socket/src/app/socket.service.ts
+++ Socket/src/app/socket.service.ts
@@ -1,5 +1,4 @@
-import { Injectable } from '@angular/core';
-import { EventEmitter } from 'protractor';
+import { Injectable, EventEmitter } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
